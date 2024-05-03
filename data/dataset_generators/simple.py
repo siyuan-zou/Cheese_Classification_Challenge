9b42b337a1a2cf4881b06d7b0dce1ed68ed4528d
siyuan-zou <zsyfdu@gmail.com>
2024-05-03 21:21:53 +0200

    add gen selling

--- data/dataset_generators/simple.py
+++ data/dataset_generators/simple.py
@@ -1,5 +1,6 @@
 from .base import DatasetGenerator
-
+import json
+# from unidecode import unidecode
 
 class SimplePromptsDatasetGenerator(DatasetGenerator):
     def __init__(
@@ -19,6 +20,33 @@ class SimplePromptsDatasetGenerator(DatasetGenerator):
             prompts[label].append(
                 {
                     "prompt": f"An image of a {label} cheese",
+                    "num_images": self.num_images_per_label,
+                }
+            )
+        return prompts
+
+class SellingPromptsGenerator(DatasetGenerator):
+    def __init__(
+        self,
+        generator,
+        batch_size=1,
+        output_dir="dataset/train",
+        num_images_per_label=200,
+    ):
+        super().__init__(generator, batch_size, output_dir)
+        self.num_images_per_label = num_images_per_label
+
+    def create_prompts(self, labels_names):
+        with open("prompts/selling.json", "r") as f:
+            selling_prompts = json.load(f)
+
+        prompts = {}
+        for label in labels_names:
+            prompts[label] = []
+            # remove accents of the label
+            prompts[label].append(
+                {
+                    "prompt": selling_prompts[label],
                     "num_images": self.num_images_per_label,
                 }
             )

d9bc8c82b8375797fcde9150d17efa72e1eb1cc5
siyuan-zou <zsyfdu@gmail.com>
2024-05-04 22:11:59 +0200

    up add foods gen

--- data/dataset_generators/simple.py
+++ data/dataset_generators/simple.py
@@ -1,5 +1,6 @@
 from .base import DatasetGenerator
 import json
+import re
 # from unidecode import unidecode
 
 class SimplePromptsDatasetGenerator(DatasetGenerator):
@@ -99,10 +100,53 @@ class ProductionLPromptsGenerator(DatasetGenerator):
         for label in labels_names:
             prompts[label] = []
             # remove accents of the label
-            prompts[label].append(
-                {
-                    "prompt": designed_prompts[label].replace(f"{label.lower()}", f"{label.lower()}" + " cheese"),
-                    "num_images": self.num_images_per_label,
-                }
-            )
+            if '-' in label:
+                # remove spaces around hyphen
+                label_cleaned = re.sub(r"\s*-\s*", "-", label)
+                # remove accents of the label
+                prompts[label].append(
+                    {
+                        "prompt": designed_prompts[label].replace(f"{label_cleaned.lower()}", f"{label_cleaned.lower()}" + " cheese"),
+                        "num_images": self.num_images_per_label,
+                    }
+                )
+            else:
+                # remove accents of the label
+                prompts[label].append(
+                    {
+                        "prompt": designed_prompts[label].replace(f"{label.lower()}", f"{label.lower()}" + " cheese"),
+                        "num_images": self.num_images_per_label,
+                    }
+                )
+
+        return prompts
+
+class FoodsPromptsGenerator:
+    def __init__(
+        self,
+        generator,
+        batch_size=1,
+        output_dir="dataset/train",
+        num_images_per_label=200,
+    ):
+        super().__init__(generator, batch_size, output_dir)
+        self.num_images_per_label = num_images_per_label
+        self.type = "foods_prompts"
+
+    def create_prompts(self, labels_names):
+        with open("/users/eleves-b/2022/siyuan.zou/DL_SiyuanZou/Chellenge_Cheese/prompts/foods.json", "r") as f:
+            designed_prompts = json.load(f)
+
+        prompts = {}
+        for label in labels_names:
+            prompts[label] = []
+            # remove accents of the label
+            for food_type in designed_prompts[label]:
+                prompts[label].append(
+                    {
+                        "prompt": label + " cheese:" + food_type,
+                        "num_images": self.num_images_per_label,
+                    }
+                )
+
         return prompts
